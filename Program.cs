ab6a5de33ffe18330e78b073b638fde0c4b25ec6
Vova Lantsov <vova.lantsov.dev@gmail.com>
2019-04-30 21:29:09 +0300

    INIT solution + project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace MaximEmmBots
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+        }
+    }
+}
