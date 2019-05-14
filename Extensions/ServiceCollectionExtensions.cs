d8f81a5f2f9d33101aae3a30f5f02e2d61a50d2c
Vova Lantsov <vova.lantsov.dev@gmail.com>
2019-05-14 21:56:42 +0300

    INIT service collection extensions

---
+++ Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+using MaximEmmBots.Models.Json;
+using MaximEmmBots.Options;
+using Microsoft.Extensions.DependencyInjection;
+using MaximEmmBots.Services;
+using Telegram.Bot;
+
+namespace MaximEmmBots.Extensions
+{
+    internal static class ServiceCollectionExtensions
+    {   
+        internal static void AddServices(this IServiceCollection services, Data data)
+        {
+            services.Configure<DataOptions>(options => options.Data = data);
+
+            services.AddSingleton(new TelegramBotClient(data.Bot.Token));
+            services.AddSingleton<Context>();
+            services.AddSingleton<BotHandler>();
+            services.AddHostedService<BotHandlerService>();
+        }
+    }
+}
