d2a9c1929ac432437acdeecd835a4ed37815c36a
Vova Lantsov <vova.lantsov.dev@gmail.com>
2019-05-17 17:30:08 +0300

    INIT distribution service

--- Extensions/ServiceCollectionExtensions.cs
+++ Extensions/ServiceCollectionExtensions.cs
@@ -1,25 +1,34 @@
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
 using MaximEmmBots.Models.Json;
 using MaximEmmBots.Options;
 using Microsoft.Extensions.DependencyInjection;
 using MaximEmmBots.Services;
-using MaximEmmBots.Services.ReviewGrabberBot;
 using Telegram.Bot;
+using ReviewBotWorkerService = MaximEmmBots.Services.ReviewGrabberBot.WorkerService;
+using DistributionBotWorkerService = MaximEmmBots.Services.DistributionBot.WorkerService;
 
 namespace MaximEmmBots.Extensions
 {
     internal static class ServiceCollectionExtensions
-    {   
-        internal static void AddServices(this IServiceCollection services, Data data)
+    {
+        internal static void AddGeneralServices(this IServiceCollection services, Data data,
+            BaseClientService.Initializer googleInitializer)
         {
             services.Configure<DataOptions>(options => options.Data = data);
 
             services.AddSingleton(new TelegramBotClient(data.Bot.Token));
             services.AddSingleton<Context>();
+            services.AddSingleton(new SheetsService(googleInitializer));
             
             services.AddSingleton<BotHandler>();
             services.AddHostedService<BotHandlerService>();
+        }
 
-            services.AddHostedService<WorkerService>();
+        internal static void AddWorkerServices(this IServiceCollection services)
+        {
+            services.AddHostedService<ReviewBotWorkerService>();
+            services.AddHostedService<DistributionBotWorkerService>();
         }
     }
 }
