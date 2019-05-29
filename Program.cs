b408da80b2df976fea9cc70bd3bb03b4f6ee5441
Vova Lantsov <vova.lantsov.dev@gmail.com>
2019-05-29 22:27:45 +0300

    FEATURE changed culture dictionaries to CultureService

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,7 +6,6 @@ using Google.Apis.Services;
 using MaximEmmBots.Extensions;
 using MaximEmmBots.Models.Json;
 using Microsoft.Extensions.Hosting;
-using TimeZoneConverter;
 
 namespace MaximEmmBots
 {
@@ -22,16 +19,8 @@ namespace MaximEmmBots
             var languageModels = SettingsExtensions.LoadLanguagesAsync(Directory.GetCurrentDirectory(),
                 data.Restaurants.Select(r => r.Culture.Name).Distinct());
             var languageDictionary = new Dictionary<string, LocalizationModel>();
-            var cultureDictionary = new Dictionary<string, CultureInfo>();
             await foreach (var (name, model) in languageModels)
-            {
                 languageDictionary[name] = model;
-                cultureDictionary[name] = new CultureInfo(name);
-            }
-
-            var timeZoneDictionary = new Dictionary<string, TimeZoneInfo>();
-            foreach (var timeZone in data.Restaurants.Select(r => r.Culture.TimeZone).Distinct())
-                timeZoneDictionary[timeZone] = TZConvert.GetTimeZoneInfo(timeZone);
 
             var googleCredential = await GoogleSheetsExtensions.AuthorizeAsync(data.GoogleCredentials);
             var googleInitializer = new BaseClientService.Initializer
@@ -48,8 +37,7 @@ namespace MaximEmmBots
                     serviceCollection.AddGoogleServices(googleInitializer);
                     serviceCollection.AddBotServices(data.Bot.Token);
                     serviceCollection.AddWorkerServices();
-                    serviceCollection.AddLocalizationServices(languageDictionary, timeZoneDictionary,
-                        cultureDictionary);
+                    serviceCollection.AddLocalizationServices(languageDictionary);
                     serviceCollection.AddHttpFactory();
                 })
                 .ConfigureLogging(LoggingExtensions.Configure)
