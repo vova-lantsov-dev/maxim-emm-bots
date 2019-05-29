5a4e8fedfb19eaf937405d8c18c18e0b4fbf754a
Vova Lantsov <vova.lantsov.dev@gmail.com>
2019-05-29 19:59:58 +0300

    FIX GetOrAdd's factory delegate is not called thread-safely

--- Services/CultureService.cs
+++ Services/CultureService.cs
@@ -8,22 +8,32 @@ namespace MaximEmmBots.Services
 {
     internal sealed class CultureService
     {
-        private readonly ConcurrentDictionary<string, CultureInfo> _cultures = new ConcurrentDictionary<string, CultureInfo>();
-        private readonly ConcurrentDictionary<string, TimeZoneInfo> _timeZones = new ConcurrentDictionary<string, TimeZoneInfo>();
+        private readonly ConcurrentDictionary<string, Lazy<CultureInfo>> _cultures = new ConcurrentDictionary<string, Lazy<CultureInfo>>();
+        private readonly ConcurrentDictionary<string, Lazy<TimeZoneInfo>> _timeZones = new ConcurrentDictionary<string, Lazy<TimeZoneInfo>>();
 
         internal CultureInfo CultureFor(Restaurant restaurant)
         {
-            return _cultures.GetOrAdd(restaurant.Culture.Name, name => new CultureInfo(name));
+            return _cultures.GetOrAdd(restaurant.Culture.Name, CultureInfoInitializer).Value;
         }
 
         internal TimeZoneInfo TimeZoneFor(Restaurant restaurant)
         {
-            return _timeZones.GetOrAdd(restaurant.Culture.TimeZone, TZConvert.GetTimeZoneInfo);
+            return _timeZones.GetOrAdd(restaurant.Culture.TimeZone, TimeZoneInitializer).Value;
         }
 
         internal DateTime NowFor(Restaurant restaurant)
         {
             return TimeZoneInfo.ConvertTime(DateTime.UtcNow, TimeZoneFor(restaurant));
+        }
+
+        private static Lazy<CultureInfo> CultureInfoInitializer(string cultureName)
+        {
+            return new Lazy<CultureInfo>(() => new CultureInfo(cultureName));
+        }
+
+        private static Lazy<TimeZoneInfo> TimeZoneInitializer(string timeZoneName)
+        {
+            return new Lazy<TimeZoneInfo>(() => TZConvert.GetTimeZoneInfo(timeZoneName));
         }
     }
 }
