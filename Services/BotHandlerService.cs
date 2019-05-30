9babd6a8cbaae4d971e7d89226ab5856cd350f62
Vova Lantsov <vova.lantsov.dev@gmail.com>
2019-05-30 11:50:15 +0300

    FEATURE logging for BotHandlerService

--- Services/BotHandlerService.cs
+++ Services/BotHandlerService.cs
@@ -1,6 +1,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 using Telegram.Bot.Extensions.Polling;
 
@@ -10,15 +11,21 @@ namespace MaximEmmBots.Services
     {
         private readonly IUpdateHandler _updateHandler;
         private readonly ITelegramBotClient _client;
+        private readonly ILogger<BotHandlerService> _logger;
         
-        public BotHandlerService(IUpdateHandler updateHandler, ITelegramBotClient client)
+        public BotHandlerService(IUpdateHandler updateHandler,
+            ITelegramBotClient client,
+            ILogger<BotHandlerService> logger)
         {
             _updateHandler = updateHandler;
             _client = client;
+            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            _logger.LogInformation("BotHandlerService started");
+            
             await _client.ReceiveAsync(_updateHandler, stoppingToken);
         }
     }
