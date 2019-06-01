98e23ff4652b6edebc103b3ef7300bfdb8d2ebaf
Vova Lantsov <vova.lantsov.dev@gmail.com>
2019-06-01 17:50:32 +0300

    FEATURE yielding restaurants from separate files

--- Extensions/SettingsExtensions.cs
+++ Extensions/SettingsExtensions.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using MaximEmmBots.Models.Json;
+using MaximEmmBots.Models.Json.Restaurants;
 
 namespace MaximEmmBots.Extensions
 {
@@ -12,6 +13,18 @@ namespace MaximEmmBots.Extensions
         {
             await using var settingsFile = File.OpenRead(settingsFilePath);
             return await JsonSerializer.ReadAsync<Data>(settingsFile);
+        }
+
+        internal static async IAsyncEnumerable<Restaurant> YieldRestaurantsAsync()
+        {
+            var dirPath = Path.Combine(Directory.GetCurrentDirectory(), "Restaurants");
+            foreach (var filePath in Directory.GetFiles(dirPath, "*.json", SearchOption.AllDirectories))
+            {
+                await using var fileWithRestaurant = File.OpenRead(filePath);
+                var restaurant = await JsonSerializer.ReadAsync<Restaurant>(fileWithRestaurant);
+                restaurant.Name = Path.GetFileNameWithoutExtension(filePath);
+                yield return restaurant;
+            }
         }
 
         internal static async IAsyncEnumerable<(string name, LocalizationModel model)> YieldLanguagesAsync(string basePath, IEnumerable<string> languageKeys)
