083b287aa356618aa9cafac5ffe1b1d6e607505a
Vova Lantsov <vova.lantsov.dev@gmail.com>
2019-07-03 12:22:55 +0300

    FEATURE static scheduler

--- Services/Scheduling/SchedulingService.cs
+++ Services/Scheduling/SchedulingService.cs
@@ -31,12 +31,13 @@ namespace MaximEmmBots.Services.Scheduling
         {
             var getSchedulingDelay = scheduler.SchedulingMode switch
             {
-                SchedulingMode.Daily => (Func<IScheduler, TimeSpan>)GetDailyDelay,
-                SchedulingMode.Weekly => (Func<IScheduler, TimeSpan>)GetWeeklyDelay,
-                SchedulingMode.Monthly => (Func<IScheduler, TimeSpan>)GetMonthlyDelay,
+                SchedulingMode.Daily => (Func<IScheduler, TimeSpan>) GetDailyDelay,
+                SchedulingMode.Weekly => (Func<IScheduler, TimeSpan>) GetWeeklyDelay,
+                SchedulingMode.Monthly => (Func<IScheduler, TimeSpan>) GetMonthlyDelay,
+                SchedulingMode.Static => (Func<IScheduler, TimeSpan>) (s => s.SchedulingTime),
                 _ => throw new ArgumentOutOfRangeException(nameof(scheduler.SchedulingMode))
             };
-            
+
             while (!_stoppingToken.IsCancellationRequested)
             {
                 try
@@ -56,7 +57,7 @@ namespace MaximEmmBots.Services.Scheduling
 
                 if (_stoppingToken.IsCancellationRequested)
                     break;
-                
+
                 try
                 {
                     await scheduler.OnElapseAsync(_stoppingToken);
@@ -80,56 +81,64 @@ namespace MaximEmmBots.Services.Scheduling
         {
             if (!(scheduler is IWeeklyScheduler weeklyScheduler))
                 throw new ArgumentException("Weekly scheduler is not of type IWeeklyScheduler", nameof(scheduler));
-            
+
             var now = _cultureService.NowFor(scheduler.Restaurant);
             var nowDayOfWeek = now.DayOfWeek == DayOfWeek.Sunday ? 7 : (int) now.DayOfWeek;
             if (scheduler.SchedulingTime > now.TimeOfDay)
                 nowDayOfWeek++;
-            
+
             var daysDelay = 7;
             foreach (var schedulingDayOfWeek in weeklyScheduler.SchedulingDaysOfWeek)
             {
-                var leftDays = schedulingDayOfWeek == nowDayOfWeek ? 0 : schedulingDayOfWeek > nowDayOfWeek
-                    ? schedulingDayOfWeek - nowDayOfWeek : schedulingDayOfWeek + 7 - nowDayOfWeek;
+                var leftDays = schedulingDayOfWeek == nowDayOfWeek
+                    ? 0
+                    : schedulingDayOfWeek > nowDayOfWeek
+                        ? schedulingDayOfWeek - nowDayOfWeek
+                        : schedulingDayOfWeek + 7 - nowDayOfWeek;
                 if (daysDelay > leftDays)
                     daysDelay = leftDays;
-                
+
                 if (daysDelay == 0)
                     break;
             }
-            
+
             return (now.TimeOfDay <= scheduler.SchedulingTime
-                ? scheduler.SchedulingTime - now.TimeOfDay
-                : TimeSpan.FromDays(1d) + scheduler.SchedulingTime - now.TimeOfDay) + TimeSpan.FromDays(daysDelay);
+                       ? scheduler.SchedulingTime - now.TimeOfDay
+                       : TimeSpan.FromDays(1d) + scheduler.SchedulingTime - now.TimeOfDay) +
+                   TimeSpan.FromDays(daysDelay);
         }
 
         private TimeSpan GetMonthlyDelay(IScheduler scheduler)
         {
             if (!(scheduler is IMonthlyScheduler monthlyScheduler))
                 throw new ArgumentException("Monthly scheduler is not of type IMonthlyScheduler", nameof(scheduler));
-            
+
             var now = _cultureService.NowFor(scheduler.Restaurant);
             var next = now.AddMonths(1);
             var nowDayOfMonth = now.Day;
             var nextDaysOfMonth = DateTime.DaysInMonth(next.Year, next.Month);
             if (scheduler.SchedulingTime > now.TimeOfDay)
                 nowDayOfMonth++;
-            
+
             var daysDelay = 32;
             foreach (var schedulingDayOfMonth in monthlyScheduler.SchedulingDaysOfMonth)
             {
-                var leftDays = schedulingDayOfMonth == nowDayOfMonth ? 0 : schedulingDayOfMonth > nowDayOfMonth
-                    ? schedulingDayOfMonth - nowDayOfMonth : schedulingDayOfMonth + nextDaysOfMonth - nowDayOfMonth;
+                var leftDays = schedulingDayOfMonth == nowDayOfMonth
+                    ? 0
+                    : schedulingDayOfMonth > nowDayOfMonth
+                        ? schedulingDayOfMonth - nowDayOfMonth
+                        : schedulingDayOfMonth + nextDaysOfMonth - nowDayOfMonth;
                 if (daysDelay > leftDays)
                     daysDelay = leftDays;
-                
+
                 if (daysDelay == 0)
                     break;
             }
-            
+
             return (now.TimeOfDay <= scheduler.SchedulingTime
                        ? scheduler.SchedulingTime - now.TimeOfDay
-                       : TimeSpan.FromDays(1d) + scheduler.SchedulingTime - now.TimeOfDay) + TimeSpan.FromDays(daysDelay);
+                       : TimeSpan.FromDays(1d) + scheduler.SchedulingTime - now.TimeOfDay) +
+                   TimeSpan.FromDays(daysDelay);
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
