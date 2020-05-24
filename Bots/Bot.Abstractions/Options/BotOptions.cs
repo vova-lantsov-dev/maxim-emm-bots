51cc6617bc2c0373783762db99a5d8101f02a58c
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2020-05-24 22:32:27 +0300

    Bot options added

---
+++ Bots/Bot.Abstractions/Options/BotOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Bot.Abstractions.Options
+{
+    public sealed class BotOptions
+    {
+        public string Token { get; set; }
+        public string Username { get; set; }
+        public string GroupReloadCommand { get; set; }
+        public string PrivateReloadCommand { get; set; }
+    }
+}
