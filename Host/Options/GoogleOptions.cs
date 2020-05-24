9da6e5831ecee3356d4ba8d14a881eb0c4bbd9e7
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2020-05-24 22:28:31 +0300

    Added configuration files

---
+++ Host/Options/GoogleOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Host.Options
+{
+    public sealed class GoogleOptions
+    {
+        [JsonPropertyName("client_id")]
+        public string ClientId { get; set; }
+        [JsonPropertyName("client_secret")]
+        public string ClientSecret { get; set; }
+    }
+}
